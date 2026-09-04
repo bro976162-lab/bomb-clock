6fbbbcb40f71075705fce291a3121da05dce47f8
bro976162-lab <bro976162@gmail.com>
2026-09-04 13:03:46 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,130 @@
+const CACHE_NAME = "future-clock-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
+];
+
+
+/* ================================
+   INSTALL
+================================ */
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+            .then(cache => {
+
+                return cache.addAll(
+                    FILES_TO_CACHE
+                );
+
+            })
+            .then(() => {
+
+                return self.skipWaiting();
+
+            })
+
+    );
+
+});
+
+
+/* ================================
+   ACTIVATE
+================================ */
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys()
+            .then(cacheNames => {
+
+                return Promise.all(
+
+                    cacheNames
+                        .filter(name =>
+                            name !== CACHE_NAME
+                        )
+                        .map(name =>
+                            caches.delete(name)
+                        )
+
+                );
+
+            })
+            .then(() => {
+
+                return self.clients.claim();
+
+            })
+
+    );
+
+});
+
+
+/* ================================
+   FETCH
+================================ */
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(cachedResponse => {
+
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(event.request)
+                    .then(response => {
+
+                        if (
+                            !response ||
+                            response.status !== 200 ||
+                            response.type !== "basic"
+                        ) {
+                            return response;
+                        }
+
+                        const responseClone =
+                            response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+
+                                cache.put(
+                                    event.request,
+                                    responseClone
+                                );
+
+                            });
+
+                        return response;
+
+                    })
+                    .catch(() => {
+
+                        return caches.match(
+                            "./index.html"
+                        );
+
+                    });
+
+            })
+
+    );
+
+});
